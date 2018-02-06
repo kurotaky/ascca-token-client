7614280165a822ede242b4e2b00108384b6f5707
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2018-02-06 10:42:57 +0900

    add tests

--- test/ascca-token-client-test.js
+++ test/ascca-token-client-test.js
@@ -3,19 +3,36 @@ import assert from 'power-assert';
 import nock from 'nock';
 import AsccaTokenClient from '../src/ascca-token-client';
 
-describe("ascca-token-client-test", function() {
+describe("ascca-token-client-test", function () {
   describe("#getToken()", () => {
-    const scope = nock("https://test.ascca.jp:2443")
-      .post("/ccstokensv/api/requestToken")
-      .reply(200, { errCode: '00', token: '0841905582174001', cardEndNo: '1111' });
+    describe("when errCode 00 returns", () => {
+      const scope = nock("https://test.ascca.jp:2443")
+        .post("/ccstokensv/api/requestToken")
+        .reply(200, { errCode: '00', token: '0841905582174001', cardEndNo: '1111' });
 
-    it("should be success", async () => {
-      var asccaTokenClient = new AsccaTokenClient("testpg0000000");
-      var response = await asccaTokenClient.getToken('4111111111111111', '2210');
-      assert(response.errCode === '00');
-      assert(response.token === '0841905582174001');
-      assert(response.cardEndNo === '1111');
-      assert(scope.isDone);
+      it("should be success", async () => {
+        var asccaTokenClient = new AsccaTokenClient("testpg0000000");
+        var response = await asccaTokenClient.getToken('4111111111111111', '2210');
+        assert(response.errCode === '00');
+        assert(response.token === '0841905582174001');
+        assert(response.cardEndNo === '1111');
+        assert.ok(scope.isDone);
+      });
+    });
+
+    describe("when errCode 43 returns", () => {
+      const scope = nock("https://test.ascca.jp:2443")
+        .post("/ccstokensv/api/requestToken")
+        .reply(200, { errCode: '43', token: null, cardEndNo: null });
+
+      it("wrong shopId should be invalid", async () => {
+        var asccaTokenClient = new AsccaTokenClient("invalidshopid");
+        var response = await asccaTokenClient.getToken('4111111111111111', '2210');
+        assert(response.errCode === '43');
+        assert(response.token === null);
+        assert(response.cardEndNo === null);
+        assert.ok(scope.isDone);
+      });
     });
   });
 });
