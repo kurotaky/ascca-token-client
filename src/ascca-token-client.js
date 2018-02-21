1cb8e65afa5000ef45b22de90bfd27ad358a792c
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2018-02-21 15:43:10 +0900

    standard

--- src/ascca-token-client.js
+++ src/ascca-token-client.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict'
 
 import fetch from 'isomorphic-fetch'
 
 export default class AsccaTokenClient {
-  constructor(shopId, options = {production: false}) {
-    this.shopId = shopId;
-    this.url = options.production ? 'https://secure.ascca.jp:2443' : 'https://test.ascca.jp:2443';
+  constructor (shopId, options = {production: false}) {
+    this.shopId = shopId
+    this.url = options.production ? 'https://secure.ascca.jp:2443' : 'https://test.ascca.jp:2443'
   }
 
-  getToken(cardNo, cardExpire) {
+  getToken (cardNo, cardExpire) {
     return fetch(this.url + '/ccstokensv/api/requestToken', {
       method: 'POST',
       headers: {
@@ -20,19 +20,19 @@ export default class AsccaTokenClient {
         cardExpire: cardExpire
       })
     }).then(function (response) {
-      return response.json();
+      return response.json()
     }).then(function (json) {
-      return json;
+      return json
     }).catch(function (error) {
-      console.log('request failed', error);
-    });
+      console.log('request failed', error)
+    })
   };
 
-  url() {
-    return this.url;
+  url () {
+    return this.url
   };
 }
 
-if (typeof window != "undefined") {
-  !window.AsccaTokenClient && (window.AsccaTokenClient = AsccaTokenClient);
+if (typeof window !== 'undefined') {
+  !window.AsccaTokenClient && (window.AsccaTokenClient = AsccaTokenClient)
 }
